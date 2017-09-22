4480e03cca7da79ee01bd49298bf9b93a88596fd
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-22 11:24:03 -0700

    Attempt to fix bug in either JS or HTML. So far, no luck.

--- js/scripts.js
+++ js/scripts.js
@@ -27,34 +27,39 @@ $(document).ready(function() {
 
   $("#pingForm").submit(function(event){
     alert("working");
-    var input= $("input#userInput").val();
+    var input= $parseInt("input#numberInput").val();
 
-    if (input.length === 1) {
-      var dog = oneFunction(input);
-      $("#output").show();
-      $("#output1").text(dog);
+    for (i = 0; i < input.length; i ++) {
+      alert("for working");
     }
 
-    if (input.length === 2) {
-      var cat = tenFunction(input);
-      $("#output").show();
-      $("#output1").text(cat);
-    }
 
-    if (input.length === 3) {
-      var fish = hundredFunction(input);
-      $("#output").show();
-      $("#output1").text(fish);
-    }
-
-    if (input.length === 4 && input <= 4999) {
-      var horse = thousandFunction(input);
-      $("#output").show();
-      $("#output1").text(horse);
-
-    } else if (input > 4999) {
-      alert("Roman numerals end at 4,999. Please enter a lower number.");
-    }
+    // if (input.length === 1) {
+    //   var dog = oneFunction(input);
+    //   $("#output").show();
+    //   $("#output1").text(dog);
+    // }
+    //
+    // if (input.length === 2) {
+    //   var cat = tenFunction(input);
+    //   $("#output").show();
+    //   $("#output1").text(cat);
+    // }
+    //
+    // if (input.length === 3) {
+    //   var fish = hundredFunction(input);
+    //   $("#output").show();
+    //   $("#output1").text(fish);
+    // }
+    //
+    // if (input.length === 4 && input <= 4999) {
+    //   var horse = thousandFunction(input);
+    //   $("#output").show();
+    //   $("#output1").text(horse);
+    //
+    // } else if (input > 4999) {
+    //   alert("Roman numerals end at 4,999. Please enter a lower number.");
+    // }
 
     event.preventDefault();
   });
