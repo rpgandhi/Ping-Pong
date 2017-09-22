79d265bda56d8bcc1a489c453cd4070c3ddf213e
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-22 12:55:37 -0700

    Try to resolve bugs where parts of the JS code is not working.

--- js/scripts.js
+++ js/scripts.js
@@ -24,43 +24,42 @@
 // });
 
 $(document).ready(function() {
-
   $("#pingForm").submit(function(event){
     alert("working");
-    var input= $parseInt("input#numberInput").val();
+    var input= $parseInt(("input#numberInput").val());
+    resultsArray = []
+    // if (input === 3){
+    //   alert("if working");
+    // }
 
-    for (i = 0; i < input.length; i ++) {
-      alert("for working");
+    for (var i = 1; i < input.length; i ++) {
+      alert("for loop working");
+      if ((i % 3)===0) {
+        resultsArray.push("ping");
+      } else if ((i % 5)===0) {
+        resultsArray.push("pong");
+      } else if ((i % 15)===0); {
+        resultsArray.push("ping-pong");
+      // } else {
+      //   result.Array.push(i);
+      // }
     }
+  }
+      $("output").text(resultsArray);
+      //       if ((input % 3) === 0) {
+      //         alert("working");
+      //         $("#output").show();
+      //         $("#output1").show();
+      //       } else if (input !% 3) {
+      //         $("#output").show(i);
+      //         // $("#output1").text(i);
+      //       }
+      //
+      // alert("for working");
 
 
-    // if (input.length === 1) {
-    //   var dog = oneFunction(input);
-    //   $("#output").show();
-    //   $("#output1").text(dog);
-    // }
-    //
-    // if (input.length === 2) {
-    //   var cat = tenFunction(input);
-    //   $("#output").show();
-    //   $("#output1").text(cat);
-    // }
-    //
-    // if (input.length === 3) {
-    //   var fish = hundredFunction(input);
-    //   $("#output").show();
-    //   $("#output1").text(fish);
-    // }
-    //
-    // if (input.length === 4 && input <= 4999) {
-    //   var horse = thousandFunction(input);
-    //   $("#output").show();
-    //   $("#output1").text(horse);
-    //
-    // } else if (input > 4999) {
-    //   alert("Roman numerals end at 4,999. Please enter a lower number.");
-    // }
+    // event.preventDefault();
 
-    event.preventDefault();
   });
+
 });
