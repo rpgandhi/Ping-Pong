791174d16b2119fae32a9278a4dff6ad26ecdeec
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-22 15:15:40 -0700

    Fix display bugs and check in before stopping until tonight.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-    alert("working");
+    // alert("working");
     var input= $("input#numberInput").val();
     var resultsArray= [""]
 
     for (var i = 1; i <= input; i ++) {
-      alert("for loop working");
+      // alert("for loop working");
 
       if ((i % 3)===0) {
         resultsArray.push("ping");
-        alert("3working");
+        // alert("3working");
       } else if ((i % 5)===0) {
         resultsArray.push("pong");
-        alert("5 working");
+        // alert("5 working");
       } else if ((i % 15)===0) {
         resultsArray.push("ping-pong");
-        alert("15 working");
+        // alert("15 working");
       } else {
         resultsArray.push(i);
-        alert("end else working");
+        // alert("end else working");
       }
     }
 
-    $("output").text(resultsArray.join(" "));
+    $("#output").text(resultsArray.join(" "));
 
 
   });
