e1845eb7ce83398c17332c08c9d0df5349a948f8
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-24 20:32:00 -0700

    Add additional styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,11 @@
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-    // alert("working");
+
     var input= $("input#numberInput").val();
     var resultsArray= [""]
 
     for (var i = 1; i <= input; i ++) {
-      // alert("for loop working");
-
-      // if ((i % 3)===0) {
-      //   resultsArray.push("ping");
-      //   // alert("3working");
-      // } else if ((i % 5)===0) {
-      //   resultsArray.push("pong");
-      //   // alert("5 working");
-      // } else if ((i % 15)===0) {
-      //   resultsArray.push("ping-pong");
-      //   // alert("15 working");
-      // } else {
-      //   resultsArray.push(i);
-      //   // alert("end else working");
-      // }
 
       if ((i % 15)===0) {
         resultsArray.push("ping-pong");
@@ -32,11 +17,10 @@ $(document).ready(function() {
         resultsArray.push(i);
       }
     }
-    for(var i = 1; i < resultsArray.length; i++){
-   $("#output").append('<li>' + resultsArray[i] + '</li>');
- }
 
-    // $("#output").text(resultsArray.join(" "));
+    for(var i = 1; i < resultsArray.length; i++) {
+      $("#output").append('<li>' + resultsArray[i] + '</li>');
+    }
 
   });
 });
